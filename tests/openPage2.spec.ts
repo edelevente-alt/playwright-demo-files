663d7fc72b9c2d3df9a942e25427820947265798
Levi Laszlo <edelevente@gmail.com>
2026-05-20 14:42:57 +0300

    deleted files with error

--- tests/openPage2.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test'
-import { PageManager } from '../PageObjects/PageManager';
-
-test ('SauceDemo E2E scenario 2', async ({page}) => {
-    const pageManager = new PageManager(page);
-
-    pageManager.instanceLoginPage().openSauceDemoPage();
-    pageManager.instanceLoginPage().closeSauceDemoPage();
-})
